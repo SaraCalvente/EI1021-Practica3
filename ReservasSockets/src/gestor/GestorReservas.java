13ca77a3f283d75a05347aaf82e8094969e389f8
Javier Segura <al416137@uji.es>
2025-10-13 13:03:20 +0200

    import map en el gestor

--- ReservasSockets/src/gestor/GestorReservas.java
+++ ReservasSockets/src/gestor/GestorReservas.java
@@ -14,6 +14,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 
